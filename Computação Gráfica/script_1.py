67805f290d7207be28b3dd718141e1e90fd47a7a
RaulMartino <83245964+RaulMartino@users.noreply.github.com>
2024-04-09 21:04:24 -0300

    Create script_1

---
+++ Computação Gráfica/script_1.py
@@ -0,0 +1,5 @@
+from utils import WindowHandler
+
+if __name__ == "__main__":
+    window_handler = WindowHandler(1280, 720, "www")
+    window_handler.main_loop()
--- Computação Gráfica/utils.py
+++ Computação Gráfica/utils.py
@@ -1,4 +1,5 @@
 import glfw
+from OpenGL.GL import *
 
 class WindowHandler:
     def __init__(self, width: int, height: int, title: str):
@@ -14,14 +15,15 @@ class WindowHandler:
         glfw.set_window_pos(self.window, 200, 150)
         glfw.make_context_current(self.window)
 
+        glClearColor(0, 0.2, 0.2, 0)
+
     def main_loop(self):
         while not glfw.window_should_close(self.window):
             glfw.poll_events()
+            glClear(GL_COLOR_BUFFER_BIT)
             glfw.swap_buffers(self.window)
         
         glfw.terminate()
 
-if __name__ == "__main__":
-    window_handler = WindowHandler(1280, 720, "www")
-    window_handler.main_loop()
+
 
